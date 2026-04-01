21bb3ec2196c9531c284d8054d3181a82afd27bc
Lucas Manuel <lucasmanuel.tech@gmail.com>
2026-04-01 11:41:01 -0600

    feat: add initial setup

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -3,10 +3,26 @@ pragma solidity ^0.8.13;
 
 import { Script, console2 } from "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract DeployAllFacetsMainnet is Script {
+
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+        ScriptTools.exportContract(fileSlug, "aaveFacet", new AaveFacet());
+        ScriptTools.exportContract(fileSlug, "psmFacet", new PSMFacet(Ethereum.DAI, Ethereum.USDC,...));
     }
+}
+
+contract DeployAllFacetsBase is Script {
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        ScriptTools.exportContract(fileSlug, "aaveFacet", new AaveFacet());
+    }
+
 }
